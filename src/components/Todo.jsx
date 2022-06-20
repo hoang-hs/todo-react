0cca2baeccdbb310d5267ea0044359987f411624
hoanglh1 <git@gitlab.citigo.com.vn:kfinance/base.git>
2022-06-20 11:27:36 +0700

    logic js

---
+++ src/components/Todo.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+import TodoForm from "./TodoForm";
+
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  const submitUpdate = (todo) => {
+    updateTodo({ id: edit.id, newValue: todo.text });
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      style={{display: 'flex'}}
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, newValue: todo.text })}
+          className="update-icon"
+        />
+      </div>
+    </div>
+  ));
+}
+
+export default Todo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = true;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const updateTodo = ({ id, newValue }) => {
+    if (!newValue || /^\s*$/.test(newValue)) {
+      return;
+    }
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.text = newValue;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>What to do today ?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
+}
+
+export default TodoList;
